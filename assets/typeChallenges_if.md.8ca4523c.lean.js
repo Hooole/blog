275a0f918feda6e7649012a19c08f7e91d1ce037
fuhl02 <fuhl02@mingyuanyun.com>
2023-10-13 18:05:12 +0800

    deploy

---
+++ assets/typeChallenges_if.md.8ca4523c.lean.js
@@ -0,0 +1,1 @@
+import{o as e,c as a,d as s}from"./app.91f19775.js";const n='{"title":"IF","description":"","frontmatter":{},"headers":[{"level":3,"title":"IF","slug":"if"}],"relativePath":"typeChallenges/if.md","lastUpdated":1695806102302}',o={},t=s('',6);o.render=function(s,n,o,c,p,d){return e(),a("div",null,[t])};export default o;export{n as __pageData};
